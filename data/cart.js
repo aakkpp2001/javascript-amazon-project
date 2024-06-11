db5e431bd6281ea8646bdbc53e0c50c8754ff079
Arkar Phyo <arkar2001211@gmail.com>
2024-06-11 16:13:39 +0630

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'))||
+[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
@@ -6,6 +7,10 @@ export let cart = [{
   quantity: 1
 }
 ];
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -24,6 +29,8 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -36,4 +43,6 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
