d948b8433ce19429cfcc0ee16d77d92efe91619c
Arkar Phyo <arkar2001211@gmail.com>
2024-06-21 16:23:15 +0630

    Save data and generate HTML for payment summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,4 @@
-export let cart = JSON.parse(localStorage.getItem('cart'))||
-[{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
   deliveryOptionId: '1'
@@ -38,7 +37,7 @@ export function addToCart(productId) {
 
 export function removeFromCart(productId){
   const newCart = [];
-
+  
   cart.forEach((cartItem) => {
     if(cartItem.productId !== productId){
       newCart.push(cartItem);
