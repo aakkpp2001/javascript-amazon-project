09586efeef9d8e6941437305ecafd42c2fa48d43
Arkar Phyo <arkar2001211@gmail.com>
2024-07-10 15:23:08 +0630

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -56,8 +56,34 @@ class Clothing extends Product {
   }
 }
 
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
 
+/*
+console.log(this);
+const object2 =  {
+  a: 2,
+  b: this.a
+};
+*/
 
+// function logThis(){
+//   console.log(this);
+// }
+// logThis();
+// logThis.call('hello');
+
+// const object3 = {
+//   a:2,
+//   method() {
+//     console.log(this);
+//     [1,2].forEach(() => {
+//       console.log(this);
+//     })
+//   }
+// };
+// object3.method();
 
 export const products = [
   {
